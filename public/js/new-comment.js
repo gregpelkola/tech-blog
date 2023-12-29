019997ae08185d136632238247e6639e28ef6d76
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-12-28 21:35:25 -0600

    new post js

--- public/js/new-comment.js
+++ public/js/new-comment.js
@@ -13,11 +13,13 @@ const newCommentFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.reload(); // When successful, reload the same page
+        document.location.reload(); 
+        // When successful, reload the same page
       } else {
         console.log('Response status:', response.status);
         console.log('Response text:', await response.text());
-        alert('Failed to create a comment.'); // When unsuccessful, show alert
+        // When unsuccessful, show alert
+        alert('Failed to create a comment.'); 
       }
     }
   };
---
+++ public/js/new-post.js
@@ -0,0 +1,29 @@
+// Create new post public/js/new-post.js
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#title-new-post').value.trim();
+    const content = document.querySelector('#content-new-post').value.trim();
+  
+    if (title && content) {
+      const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // When successful, load the dashboard page
+        document.location.replace('/dashboard'); 
+      } else {
+        // When unsuccessful, show alert
+        alert('Failed to create a new post.'); 
+      }
+    }
+  };
+  
+  // Event listeners
+  const newPostForm = document.querySelector('.new-post-form');
+  if (newPostForm) {
+    newPostForm.addEventListener('submit', newPostFormHandler);
+  }
