98eb091439b0a02595bd52d518b6e56430a1b4f2
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-12-31 15:10:04 -0600

    auth util

--- seeds/postData.js
+++ seeds/postData.js
@@ -3,28 +3,28 @@ const { Post } = require("../models");
 const postData = [
   {
     title: "First Blog Post",
-    content: "This is the content of the first blog post.",
+    content: "This is the first blog post.",
     user_id: 1,
   },
 
   {
     title: "Second Blog Post",
-    content: "This is the content of the second blog post.",
+    content: "This is the second blog post.",
     user_id: 2,
   },
   {
     title: "Third Blog Post",
-    content: "This is the content of the third blog post.",
+    content: "This is the third blog post.",
     user_id: 3,
   },
   {
     title: "Fourth Blog Post",
-    content: "This is the content of the fourth blog post.",
+    content: "This is the fourth blog post.",
     user_id: 4,
   },
   {
     title: "Fifth Blog Post",
-    content: "This is the content of the fifth blog post.",
+    content: "This is the fifth blog post.",
     user_id: 5,
   }
 ];
