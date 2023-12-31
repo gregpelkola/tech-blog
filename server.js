b9c76844ccc13b5ef237e7842f7a957fe34fa13f
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-12-31 16:33:36 -0600

    debug sequelize

--- server.js
+++ server.js
@@ -10,42 +10,37 @@ const hbs = exphbs.create({ helpers: require("./utils/helpers") });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Setting up session object with cookie, store and secret
+// Setting up session object with cookie, store, and secret
 const sess = {
-    secret: 'secret',
-    cookie: {},
+    secret: process.env.SECRET || 'techblog23', // Use process.env.SECRET if available, else use 'secret'
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24, // Set the maximum age of the cookie (e.g., 1 day)
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize,
+        db: sequelize,
     }),
 };
 
 // Using session middleware with object
 app.use(session(sess));
 
-//Parsing url-encoded data and JSON
+// Parsing url-encoded data and JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // Serving static files from public
 app.use(express.static("public"));
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// Seesion middleware with new object
-app.use(
-    session({
-      secret: process.env.SECRET,
-      store: new SequelizeStore({ db: sequelize }),
-      resave: false,
-      saveUninitialized: false,
-    })
-);
-
-// Uses routes from controller
+// Routes middleware
 app.use(routes);
-// Sync sequelize models with database
+
+// Sync sequelize models with the database
 sequelize.sync({ force: false }).then(() => {
-  // Start server
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+    // Start server
+    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 });
