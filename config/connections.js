d77fe302222bc57c91e4289bec705a0fe61d60a1
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-01-14 13:01:04 -0600

    .env file added, and fixed typo issue for process.env.DB_PW to DB_PASSWORD

--- config/connections.js
+++ config/connections.js
@@ -1,15 +1,16 @@
 const Sequelize = require('sequelize');
-// Import data from .env
-require('dotenv').config();
+
 
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
-        dialect: 'mysql',
+        dialect: 'mysql', 
         dialectOptions: {
-        decimalNumbers: true,
-      },
+            decimalNumbers: true,
+        },
     });
+
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
